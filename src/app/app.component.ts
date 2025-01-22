7378eba6fba7ce4b00b04f6500efdca8c38a3e1e
Vincent Leclerc <vleclerc.dev@gmail.com>
2025-01-22 14:55:12 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'dating-front';
+export class AppComponent implements OnInit {
+  // prefer inject() syntax over constructor injection
+  // alow us to extend the class without changing the constructor
+  // inject() work only on constructor phase
+  http= inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    // http get return an Observable of the response body
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.users = response;
+      },
+      error: (error) => {
+        // ex: CORS error
+        console.log(error);
+      },
+      complete: () => {
+        console.log('completed');
+      }
+    });
+  }
 }
